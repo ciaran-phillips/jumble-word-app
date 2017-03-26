059308e07f11ca98fa0d750b537a45057ef82ac8
Ciaran Phillips <ciaran@ciaranphillips.com>
2017-03-26 11:47:01 +0000

    Change API Service methods to work asynchronously

--- app/api/api.service.js
+++ app/api/api.service.js
@@ -1,23 +1,40 @@
 module.exports = ['firebaseService', apiService];
 
+
 function apiService(firebaseService) {
     
         this.rootReference = firebaseService.database().ref();
-        this.database = {};
+        this.database = null;
+
+        // If data is requested before we have it back from the API, 
+        // we queue the requests here
+        this.queuedActions = [];
+
         this.rootReference.on('value', (snapshot) => {
             this.database = snapshot.val();
-            console.log(this.database);
+            this.processQueuedActions();
         });
 }
+
 apiService.prototype.getFullDatabase = function() {
     return this.database;
 }
-apiService.prototype.getWords = function() {
-    return this.database.words;
+
+apiService.prototype.getWords = function(callback) {
+    if (this.database !== null) {
+        const words = Object.keys(this.database.words);
+        callback(words);
+    }
+    this.queueAction(this.getWords, callback);
 }
+
 apiService.prototype.getScores = function() {
-    return this.database.scores;
+    if (this.database !== null) {
+        callback(this.database.scores);
+    }
+    this.queueAction(this.getScores, callback);
 }
+
 apiService.prototype.addScore = function(name, score) {
     const scoreId = this.rootReference.child('scores').push().key;
     const scoreReference = "/scores/" + scoreId;
@@ -29,4 +46,17 @@ apiService.prototype.addScore = function(name, score) {
         }
     }
     return this.rootReference.update(scoreUpdate);
+}
+
+apiService.prototype.processQueuedActions = function() {
+    this.queuedActions.forEach((action) => {
+        action.apiCall.call(this, action.callback);
+    });
+}
+
+apiService.prototype.queueAction = function(apiCall, callback) {
+    this.queuedActions.push({
+        apiCall,
+        callback
+    });
 }
--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -1,7 +1,10 @@
 module.exports = ['$scope', 'apiService', GameController];
 
 function GameController($scope, apiService) {
-    $scope.data = apiService.getFullDatabase();
+    $scope.data = [];
+    apiService.getWords((words) => {
+        $scope.data = words;
+    });
     console.log($scope.data);
     
 }
